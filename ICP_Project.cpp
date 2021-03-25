a3e0bd72ba120aaab11ba77b5ec50344fcf0eb26
ansh3006 <76246019+ansh3006@users.noreply.github.com>
2021-03-25 23:33:14 +0530

    Update ICP_Project.cpp

--- ICP_Project.cpp
+++ ICP_Project.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 char choice;
-int dew[10]={0,0,0,0,0,0,0,0,0,0};
 int secret_pin;
 int person;
+int due[10]={0,0,0,0,0,0,0,0,0,0};
 long long mob_num[10]={9713716557,7357974302,9425794225,9977107105,7089973646,7015935333,8295243455,7894561230,110022334447,9876543210};
 long long balance[10]={50000,100500,5000,55000,1000,789,1522,252,19822,17000};
 string name[20]={"Ankit Sharma","Anshul Ghanghoria","Ankur Goud","Anshul Singh Tomar","Ansh Saxena","Rohan Shukla","Maruf Qureshi","Aman Dubey","Aditya Nema","Gaurav Tiwari"};
@@ -93,7 +93,7 @@ int main(){
     }
     
 
-    E:cout<<"--------------------Thank You-----------------!!"<<endl<<endl;
+    E:cout<<"--------------------Thank You!!-----------------"<<endl<<endl;
 
 return 0;
 }
@@ -122,24 +122,26 @@ void withdrawal(int a){
     cin>>type;
     system("cls");
     switch(type){
-        case 1: cout<<"Type The Amount You Wanna Withdraw: ";
+        case 1: cout<<"Type The Amount You Wanna Withdraw: Rs.";
                 cin>>amount;
                 if(amount <= balance[a]){
                     cout<<"Please Collect Your Cash."<<endl;
                     balance[a] -= amount;
-                    cout<<"Balance Remaining In Your Account: "<<balance[a]<<endl<<endl;
+                    cout<<"Balance Remaining In Your Account: Rs."<<balance[a]<<endl<<endl;
                     break;
                 }else{
                     cout<<"Balance Insufficient."<<endl;
-                    cout<<"Balance In Your Account: "<<balance[a]<<endl<<endl;
+                    cout<<"Balance In Your Account: Rs."<<balance[a]<<endl<<endl;
                     break;
                 }
 
-        case 2: cout<<"Type The Amount You Wanna Withdraw: ";
+        case 2: cout<<"Type The Amount You Wanna Withdraw (Less Than The Limit:5000): Rs.";
                 cin>>amount;
-                if(dew[a]<=5000 && amount<=5000){
+                if(amount<=5000){
+                    due[a] += amount;
+                }
+                if(due[a]<=5000 && amount<=5000){
                     cout<<"Please Collect Your Cash."<<endl<<endl;
-                    dew[a] += amount;
                     break;
                 }else{
                     cout<<"You Exceeded The Limit."<<endl<<endl;
@@ -155,24 +157,24 @@ void withdrawal(int a){
 }
 
 void account_balance(int a){
-    cout<<"Your Account Balance is: "<<balance[a]<<endl<<endl;
+    cout<<"Your Account Balance is: Rs."<<balance[a]<<endl<<endl;
     end();
 }
 
 void deposit_amount(int a){
     int deposit;
-    cout<<"Type The Amount You Wanna Deposit: ";
+    cout<<"Type The Amount You Wanna Deposit: Rs.";
     cin>>deposit;
     cout<<endl<<"The Amount is Successfully Deposited."<<endl;
     balance[a] += deposit;
-    cout<<"Your Account Balance Now: "<<balance[a]<<endl<<endl;
+    cout<<"Your Account Balance Now: Rs."<<balance[a]<<endl<<endl;
     end();
 }
 
 void transfer_amount(int a){
     C:string s1,s2,transfer;
     int transfer_amt,yes,transfer_person;
-    cout<<"Enter The Name Of The Account Holder To Whom You Wanna Transfer Amount: ";
+    cout<<"Enter The Full Name Of The Account Holder To Whom You Wanna Transfer Amount: ";
     cin>>s1>>s2;
     transfer = s1 + ' ' + s2;
     for(int j=0;j<10;j++){
@@ -184,22 +186,22 @@ void transfer_amount(int a){
         }
     }
     if(yes==0){
-        cout<<"Please Enter Correct Account Holder Name..."<<endl;
+        cout<<"Please Enter Correct Account Holder Name."<<endl;
         system("pause");
         system("cls");
         goto C;
     }
-    cout<<"Type The Amount You Wanna Transfer: ";
+    cout<<"Type The Amount You Wanna Transfer: Rs.";
     cin>>transfer_amt;
     cout<<endl;
     if(transfer_amt <= balance[a]){
         cout<<"Transaction Sucessful!!"<<endl;
         balance[a] -= transfer_amt;
-        cout<<"Balance Remaining In Your Account: "<<balance[a]<<endl<<endl;
+        cout<<"Balance Remaining In Your Account: Rs."<<balance[a]<<endl<<endl;
         balance[transfer_person] += transfer_amt;
     }else{
         cout<<"Transaction Unsucessful...Insufficient Balance."<<endl;
-        cout<<"Balance In Your Account: "<<balance[a]<<endl<<endl;
+        cout<<"Balance In Your Account: Rs."<<balance[a]<<endl<<endl;
     }
     end();
 }
@@ -208,7 +210,7 @@ void account_details(int a){
     cout<<"Account Holder ---"<<endl<<endl;
     cout<<"Name: "<<name[a]<<endl;
     cout<<"Mobile Number: "<<mob_num[a]<<endl;
-    cout<<"Account Balance: "<<balance[a]<<endl<<endl;
+    cout<<"Account Balance: Rs."<<balance[a]<<endl<<endl;
     end();
 }
 
